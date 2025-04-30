c693ee165f2aeda2bc131662ddac9181649ce4ea
Michael Dinerstein <md@missionmike.dev>
2025-04-29 23:15:04 -0700

    fix: blog post components.

--- src/components/mdx-components.ts
+++ src/components/mdx-components.ts
@@ -1,8 +1,12 @@
 import Aside from './blog/Aside.astro';
 import CodeHeader from './blog/CodeHeader.astro';
+import HorizontalRule from './blog/HorizontalRule.astro';
+import Image from './blog/Image.astro';
 import type { MDXComponents } from 'mdx/types';
 
 export const components: MDXComponents = {
   Aside,
   CodeHeader,
+  HorizontalRule,
+  Image,
 };
