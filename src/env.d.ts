9fcdc07add5ae1776c980efb9767db54d9f8087a
Michael Dinerstein <md@missionmike.dev>
2025-04-30 13:52:02 -0700

    fix: typescript checks.

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,3 +7,20 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module '*.astro' {
+  import type { ComponentType } from 'react';
+  const Component: ComponentType;
+  export default Component;
+}
+
+// Add this to ensure TypeScript checks .astro files
+declare module 'astro:content' {
+  interface Render {
+    '.md': Promise<{
+      Content: import('astro').MarkdownInstance<{}>['Content'];
+      headings: import('astro').MarkdownHeading[];
+      remarkPluginFrontmatter: Record<string, any>;
+    }>;
+  }
+}
