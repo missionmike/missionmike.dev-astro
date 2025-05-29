33a6646b2600d4f46abb9308867f6f1cfca57218
Michael Dinerstein <md@missionmike.dev>
2025-05-28 22:57:29 -0700

    feat: setup projects page.

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,6 +14,23 @@ const posts = defineCollection({
   }),
 });
 
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    excerpt: z.string().optional(),
+    summary: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    updated: z.string().optional(),
+    featuredImage: z.string().optional(),
+    draft: z.boolean().optional(),
+    gitHubUrl: z.string().optional(),
+    order: z.number().optional(),
+  }),
+});
+
 export const collections = {
   posts,
+  projects,
 };
